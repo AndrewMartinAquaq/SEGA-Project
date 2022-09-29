10343bf165a17560f9c420b9dae4393bb8ac5fe1
AndrewMartinAquaq <andrew.martin@aquaq.co.uk>
2022-09-29 12:08:41 +0000

    initial commit of spring boot project

---
+++ src/main/java/com/aquaq/project/SEGAProject/SegaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aquaq.project.SEGAProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegaProjectApplication.class, args);
+	}
+
+}
