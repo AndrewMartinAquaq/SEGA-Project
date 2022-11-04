1f18a31b79d372ac208f7c89778ba107df59ae07
Andrew Martin <andrew.martin@aquaq.co.uk>
2022-11-04 08:42:34 +0000

    Added additional exception handlers for some edge cases

--- src/main/java/com/aquaq/project/SEGAProject/rest/RestExceptionHandler.java
+++ src/main/java/com/aquaq/project/SEGAProject/rest/RestExceptionHandler.java
@@ -5,8 +5,10 @@ import com.aquaq.project.SEGAProject.rest.exceptions.InvalidInputException;
 import com.aquaq.project.SEGAProject.rest.exceptions.RecordNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -66,6 +68,38 @@ public class RestExceptionHandler {
         GenericErrorResponse error = new GenericErrorResponse();
 
         String message = "All fields are required";
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(message);
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<GenericErrorResponse> handleException(HttpMessageNotReadableException e){
+
+        logger.info("Expected exception handled - exception of type: " + e.getClass().getName());
+
+        GenericErrorResponse error = new GenericErrorResponse();
+
+        String message = "Value must be a number and valid";
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(message);
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<GenericErrorResponse> handleException(DataIntegrityViolationException e){
+
+        logger.info("Expected exception handled - exception of type: " + e.getClass().getName());
+
+        GenericErrorResponse error = new GenericErrorResponse();
+
+        String message = "Value must be a between 0 and 20";
 
         error.setStatus(HttpStatus.BAD_REQUEST.value());
         error.setMessage(message);
